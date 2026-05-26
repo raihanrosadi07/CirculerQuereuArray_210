a6c089f2579d6f1b77544dd93657283798442bac
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-26 11:10:19 +0700

    case 2

--- CirculerQuereu.cpp
+++ CirculerQuereu.cpp
@@ -149,6 +149,11 @@ int main()
                 q.insert();
                 break;
             } 
+            case '2':
+            {
+                q.remove();
+                break;
+            }
             }
         }
     }
