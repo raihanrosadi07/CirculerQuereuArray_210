bcaae65a0accf5fa875ce5db1e97fce1d72953b7
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-26 11:04:12 +0700

    void remove

--- CirculerQuereu.cpp
+++ CirculerQuereu.cpp
@@ -49,4 +49,14 @@ public:
         queue_array[REAR] = num;
     }
 
+    void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+
+    }
 };
