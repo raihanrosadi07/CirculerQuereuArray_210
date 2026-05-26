6c3317edfa078407b915f3770f43f2f380804e6f
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-26 11:06:45 +0700

    else vodi display

--- CirculerQuereu.cpp
+++ CirculerQuereu.cpp
@@ -100,5 +100,26 @@ public:
             }
             cout << endl;
         }
+        else
+        {
+            // jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+           while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+           
+            FRONT_position = 0;
+
+            // iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        
     }
 };
+
