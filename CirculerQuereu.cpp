365e97eee7bcd187c429eeb89cd880d2b374b3ea
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-05-26 11:10:51 +0700

    case 3

--- CirculerQuereu.cpp
+++ CirculerQuereu.cpp
@@ -154,7 +154,12 @@ int main()
                 q.remove();
                 break;
             }
+            case '3':
+            {
+                q.display();
+                break;
             }
+        }
         }
     }
 }
